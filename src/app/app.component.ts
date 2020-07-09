93695f482ab237b976d17b203a097be06e249355
Johnnypham12373 <58641460+Johnnypham12373@users.noreply.github.com>
2020-07-09 15:57:09 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { ElementRef, ViewChild } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { NotesService } from './services/notes.service';
+import { AlertController, NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +19,11 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public notesService: NotesService,
+    private alertCtrl: AlertController,
+    private navCtrl: NavController,
+    private route: ActivatedRoute
   ) {
     this.initializeApp();
   }
@@ -27,16 +34,83 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
-
-  ngAfterViewInit() {
-    this.alanBtnComponent.nativeElement.addEventListener('command', (data) => {
-        const commandData = (<CustomEvent>data).detail;
-
-        if (commandData.command === 'navigation') {
-            //call client code that will react on the received command
+  addNote(){
+ 
+    this.alertCtrl.create({
+      header: 'New Note',
+      message: 'What should the title of this note be?',
+      inputs: [
+        {
+          type: 'text',
+          name: 'title'
         }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            this.notesService.createNote(data.title);
+          }
+        }
+      ]
+    }).then((alert) => {
+      alert.present();
     });
-}
+ 
+  }
+  
+  note = {
+    id: '',
+    title: '',
+    content: ''
+  };
+  ngOnInit() {
+
+    // Get the id of the note from the URL
+    let noteId = this.route.snapshot.paramMap.get('id');
+
+    // Check that the data is loaded before getting the note
+    // This handles the case where the detail page is loaded directly via the URL
+    if(this.notesService.loaded){
+      this.note = this.notesService.getNote(noteId)
+    } else {
+      this.notesService.load().then(() => {
+        this.note = this.notesService.getNote(noteId)
+      });
+    }}
+
+  noteChanged(){
+    this.notesService.save();
+  }
+
+  deleteNote(note){
+    this.notesService.deleteNote(this.note);
+    this.navCtrl.navigateBack('/notes');
+  }
+
+  ngAfterViewInit(): void {
+    this.alanBtnComponent.nativeElement.addEventListener('command', (data) => {
+      const commandData = (<CustomEvent> data).detail;
+ 
+      if(commandData.command === 'addNote') {
+        console.log('Opening alert')
+        this.addNote();
+      }
+      
+      if(commandData.command === 'addTitle') {
+        this.notesService.createNote(commandData.name)
+        this.alertCtrl.dismiss();
+      }
+      
+      if(commandData.command === 'deleteNote') {
+        this.deleteNote(commandData.note);
+      }
+
+    })
+  }
 
 }
 
